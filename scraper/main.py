bb4afa3e2eec9310826f2cdc29f733f3493565bc
Iwona Bendig <81482747+iben0402@users.noreply.github.com>
2023-12-10 15:32:40 +0100

    saving categories to xml file
    note that xml file is not finished. I will upload finished file later

--- scraper/main.py
+++ scraper/main.py
@@ -6,6 +6,31 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import re
+import xml.etree.ElementTree as ET
+import os
+
+
+def append_to_xml(product_details, xml_file_path):
+    # Check if the XML file exists
+    if not os.path.exists(xml_file_path):
+        # Create the root element
+        root = ET.Element('Products')
+        tree = ET.ElementTree(root)
+    else:
+        # Parse the existing XML file
+        tree = ET.parse(xml_file_path)
+        root = tree.getroot()
+
+    # Create a new element for this product
+    product_element = ET.SubElement(root, 'Product')
+
+    # Add details to the product element
+    for key, value in product_details.items():
+        sub_element = ET.SubElement(product_element, key)
+        sub_element.text = str(value)
+
+    # Write back to the XML file
+    tree.write(xml_file_path)
 
 
 def scrape_product_page(driver, product_page_url):
@@ -23,20 +48,22 @@ def scrape_product_page(driver, product_page_url):
 
     # Clean up the price using regular expression
     product_price = re.sub(r'[^\d.,]', '', product_price_raw)
-    print(f"Product Price: {product_price}")
+    # print(f"Product Price: {product_price}")
 
+    specifications = ""
     # Alternative method if the first one fails
     try:
         attributes_table = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, 'table.woocommerce-product-attributes.shop_attributes'))
         )
         attribute_rows = attributes_table.find_elements(By.CSS_SELECTOR, 'tbody tr')
-        print("Specifications:")
+        # print("Specifications:")
         for row in attribute_rows:
             attribute_name = row.find_element(By.CSS_SELECTOR, 'span.wd-attr-name').text.strip()
             attribute_value = row.find_element(By.CSS_SELECTOR,
                                                'td.woocommerce-product-attributes-item__value').text.strip()
-            print(f"{attribute_name}: {attribute_value}")
+            # print(f"{attribute_name}: {attribute_value}")
+            specifications += f"{attribute_name}: {attribute_value}\n"
 
     except Exception:
         print("No specifications found in the second method.")
@@ -53,29 +80,30 @@ def scrape_product_page(driver, product_page_url):
 
         # Now, specifications_html contains the HTML content
         # You can print it or save it to a file
-        print(specifications_html)
-
-        # If you want to save it to a file
-        # with open('specifications.html', 'w', encoding='utf-8') as file:
-        #     file.write(specifications_html)
+        # print(specifications_html)
 
     except Exception:
         print("No specifications found in the first method.")
+    finally:
+        return [product_price, specifications, specifications_html]
+
+def sanitize_filename(filename):
+    invalid_chars = '<>:"/\\|?*'
+    for char in invalid_chars:
+        filename = filename.replace(char, '')
+    return filename
 
 def download_image(url, save_path):
     response = requests.get(url)
     if response.status_code == 200:
         with open(save_path, 'wb') as file:
             file.write(response.content)
-        print(f"Image downloaded successfully and saved at: {save_path}")
-    else:
-        print(f"Failed to download image. Status code: {response.status_code}")
 
 
-def main():
+def scrap_products_from_category(url):
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
-    url = 'https://spawanie.com/kategoria/agregaty-pradotworcze/agregaty-trojfazowe/'
+    xml_file_path = '../scrapped_data/products.xml'
 
     try:
         driver.get(url)
@@ -100,6 +128,8 @@ def main():
             product_name = product.find_element(By.CSS_SELECTOR, '.top-information h3.wd-entities-title a').text.strip()
             product_page_link = product.find_element(By.CSS_SELECTOR,
                                                      '.top-information h3.wd-entities-title a').get_attribute('href')
+            category = scrap_category_name(product_page_link)
+            # print(category)
             srcset = product.find_element(By.CSS_SELECTOR,
                                           'a img').get_attribute('srcset')
             srcset = srcset.split(',')
@@ -108,14 +138,67 @@ def main():
                 srcset[i] = srcset[i][-2]
             photo_link_1 = srcset[0]
             photo_link_2 = srcset[len(srcset) - 2]
-            download_image(photo_link_1, f"images/{product_name}_1.jpg")
-            download_image(photo_link_2, f"images/{product_name}_2.jpg")
-            print(f"Product Name: {product_name}, Page Link: {product_page_link}")
+            file_name = sanitize_filename(product_name)
+            download_image(photo_link_1, f"../scrapped_data/images/{file_name}_1.jpg")
+            download_image(photo_link_2, f"../scrapped_data/images/{file_name}_2.jpg")
+            # print(f"Product Name: {product_name}, Page Link: {product_page_link}")
 
-            scrape_product_page(driver, product_page_link)
+            details = scrape_product_page(driver, product_page_link)
+            if (details[0] != ""):
+                product_details = {
+                    'Name': product_name,
+                    'Category': category,
+                    'ImageName1': f"{file_name}_1.jpg",
+                    'ImageName2': f"{file_name}_2.jpg",
+                    'Price': details[0],
+                    'Specifications': details[1],
+                    'SpecificationsHTML': details[2],
+                    'Link': product_page_link
+                }
+
+                append_to_xml(product_details, xml_file_path)
+
+
 
     finally:
         driver.quit()
+        print(f"saved category from url: {url} succesfully")
+
+
+def scrap_category_name(url):
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
+    try:
+        driver.get(url)
+
+        # Wait for the breadcrumb element to be present
+        breadcrumb_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, ".breadcrumb-link.breadcrumb-link-last"))
+        )
+
+        # Extract the text from the breadcrumb element
+        breadcrumb_text = breadcrumb_element.text
+
+    finally:
+        driver.quit()
+
+    return breadcrumb_text
+
+
+def main():
+    # amount of pages for each category
+    lengths = [3, 5, 9, 2, 31, 3, 5, 36, 13, 34, 3, 7]
+    with open("main_categories_urls.txt", 'r') as file:
+        urls = file.readlines()
+        idx = 0
+        for url in urls:
+            url = url[:-2]
+            length = lengths[idx]
+            for i in range(length):
+                page = f"{url}/page/{i+1}"
+                scrap_products_from_category(page)
+        idx +=1
 
 
 if __name__ == "__main__":
---
+++ scraper/readme.md
@@ -0,0 +1,15 @@
+# WAŻNE
+przed uruchomieniem scrappera należy:
+- usunąć plik products.xml
+- dodatkowo można usunąć zawartość folderu images.
+- Ważne żeby struktura plików wyglądała następująco
+
+scrapper <br>
+|--main.py <br>
+|--scrap_categories.py <br>
+|--main_categories_urls <br>
+|--subcategories.txt <br>
+|--subsubcategories.txt <br>
+|--readme.md <br>
+scrapped_data <br>
+|--images
