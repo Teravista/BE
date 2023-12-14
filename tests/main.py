34674aa337a833d2e9e45e83b169fb622d14c180
mikibak <mikibak123@gmail.com>
2023-12-14 02:08:32 +0100

    Fix adding out of stock products

--- tests/main.py
+++ tests/main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -16,17 +17,25 @@ def a_add_products(category):
         action.move_to_element(cat1).click().perform()
 
         number_of_products = 0
-        while(number_of_products < 5):
+        needed_number_of_products = 5
+        while(number_of_products < needed_number_of_products):
             products = driver.find_elements(by=By.CLASS_NAME, value="product-title")
             products[number_of_products].find_element(by=By.TAG_NAME, value="a").click()
-            if len(driver.find_elements(by=By.CLASS_NAME, value="product-flag out_of_stock")) != 0:
-                # this product is out of stock
-                continue
             additional_product_amount = random.randint(0, 4)
             for j in range(additional_product_amount):
                 driver.find_element(by=By.CLASS_NAME, value="touchspin-up").click()
-            driver.find_element(by=By.CLASS_NAME, value="add-to-cart").click()
+            time.sleep(2)
+            add_to_cart = driver.find_element(by=By.CLASS_NAME, value="add-to-cart")
+            if add_to_cart.get_property("disabled"):
+                # out of stock
+                number_of_products += 1
+                needed_number_of_products += 1
+                driver.back()
+                driver.back()
+                continue
+            add_to_cart.click()
             number_of_products += 1
+            driver.back()
             driver.back()
     except:
         print("Error:   a) Adding products to cart")
@@ -57,7 +66,7 @@ def b_search():
         print("Error:   b) Searching for products")
     else:
         print("Success: a) Searching for products")
-
+        time.sleep(2)
 
 def c_remove_products():
     try:
@@ -68,6 +77,8 @@ def c_remove_products():
         print("Error:   c) Removing products")
     else:
         print("Success: c) Removing products")
+        time.sleep(2)
+
 
 def d_register():
     try:
@@ -79,7 +90,7 @@ def d_register():
 
         email_randomizer = random.randint(0,99999999)
         driver.find_element(By.NAME, "email").send_keys("mamaszdziubale" + str(email_randomizer) + "@gmail.com" )
-        driver.find_element(By.NAME, "password").send_keys("1234")
+        driver.find_element(By.NAME, "password").send_keys("kochamako1234!")
         driver.find_element(By.NAME, "birthday").send_keys("1969-01-01")
 
         # RODO
@@ -91,6 +102,7 @@ def d_register():
         print("Error:   d) Registering")
     else:
         print("Success: d) Registering")
+        time.sleep(2)
 
 def e_order_and_status():
     try:
@@ -102,7 +114,6 @@ def e_order_and_status():
         driver.find_element(By.NAME, "confirm-addresses").click()
         driver.find_element(By.NAME, "confirmDeliveryOption").click()
         driver.find_element(By.NAME, "conditions_to_approve[terms-and-conditions]").click()
-        driver.find_element(By.NAME, "payment-option").click()
         driver.find_element(by=By.ID, value="payment-confirmation").find_element(by=By.CLASS_NAME, value="btn-primary").click()
         check_status()
     except:
@@ -120,6 +131,7 @@ def check_status():
         print("Error:   j) Checking status")
     else:
         print("Success: j) Checking status")
+        time.sleep(2)
 
 if __name__ == "__main__":
     host = 'http://localhost:8080/index.php'
